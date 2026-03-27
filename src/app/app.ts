8a6747877624cbeed5a74792f2d9bdd1ffe8d1b6
Roman Yanivets <yanivetsroma@gmail.com>
2026-03-27 15:26:14 +0100

    feat: add home page with recipes grid and header component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Header } from './component/header/header';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('cook-project');
+  title = 'cook-project';
+  showHeader = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.showHeader = !event.url.includes('/error');
+      });
+  }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import recettesData from '../../../../db.json';
+
+export interface Recette {
+  id: number;
+  description: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home implements OnInit {
+  recettes: Recette[] = [];
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.recettes = recettesData.slice(0, 4);
+  }
+
+  navigateToRecettes(): void {
+    this.router.navigate(['/recettes']);
+  }
+
+  navigateToRecette(id: number): void {
+    this.router.navigate(['/recettes', id]);
+  }
+}
