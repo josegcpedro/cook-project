b81bd02f4b868c96fdb1b66b2ea78952be742574
maryam-amn <meryem.2005.aman@gmail.com>
2026-04-03 09:13:56 +0200

    fix: correect a test for the Ci

--- src/app/pages/description-recette/description-recette.spec.ts
+++ src/app/pages/description-recette/description-recette.spec.ts
@@ -1,18 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DescriptionRecette } from './description-recette';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('DescriptionRecette', () => {
   let component: DescriptionRecette;
   let fixture: ComponentFixture<DescriptionRecette>;
 
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DescriptionRecette],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: '1' }),
+            snapshot: { paramMap: convertToParamMap({ id: '1' }) },
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DescriptionRecette);
     component = fixture.componentInstance;
+    fixture.detectChanges();
     await fixture.whenStable();
   });
 
