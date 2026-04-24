7b84861ac1348d3529614cac316cbd239e901dce
Pret@HAVANA <Havana@gmail.com>
2026-04-24 14:39:35 +0200

    Add filter system

--- src/app/pages/recettes/recettes.ts
+++ src/app/pages/recettes/recettes.ts
@@ -1,30 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import recettesData from '../../../../db.json';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
+import { FormsModule } from '@angular/forms';
 
 export interface Recette {
   id: number;
   description: string;
   image: string;
+
+  dish_type: string;
+  cuisine_type: string;
+  cooking_method: string;
+  region: string;
+  flavor: string;
 }
 
 @Component({
   selector: 'app-recettes',
-  imports: [CommonModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, FormsModule],
   templateUrl: './recettes.html',
   styleUrl: './recettes.scss',
 })
 export class Recettes implements OnInit {
+
+  // Texte saisi pour filtrer les recettes
+  searchText: string = '';
+
+  // Filtres sélectionnés par l'utilisateur
+  typePlat: string = '';
+  typeCuisine: string = '';
+  modeCuisson: string = '';
+  region: string = '';
+  sucreSale: string = '';
+
   recettes: Recette[] = [];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.recettes = recettesData.slice(0, 10);
+
+    // Récupère la recherche depuis l’URL
+    this.route.queryParams.subscribe(params => {
+      this.searchText = params['search'] || '';
+      // Ouvre les filtres si ?showfilters=true
+      this.showFilters = params['showFilters'] === 'true';
+    });
+  }
+
+  // Filtres cachés au chargement
+  showFilters = false;
+
+  toggleFilters() {
+    this.showFilters = !this.showFilters;
+  }
+
+  // Retourne les recettes filtrées selon le texte saisi
+  get filteredRecettes() {
+    const search = this.searchText.toLowerCase().trim();
+
+    return this.recettes.filter(r =>
+      r.description.toLowerCase().includes(search) &&
+      (this.typePlat === '' || r.dish_type === this.typePlat) &&
+      (this.typeCuisine === '' || r.cuisine_type === this.typeCuisine) &&
+      (this.modeCuisson === '' || r.cooking_method === this.modeCuisson) &&
+      (this.region === '' || r.region === this.region) &&
+      (this.sucreSale === '' || r.flavor === this.sucreSale)
+    );
   }
 
   navigateToRecette(id: number): void {
